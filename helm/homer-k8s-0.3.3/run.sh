b703be0a1b3f7692e7f86ea05b6943bc8cf9f903
jyje <jyjeon@outlook.com>
2025-01-29 21:42:52 +0900

    ✨ homer: bumps homer-k8s to 0.3.4

--- helm/homer-k8s-0.3.3/run.sh
+++
@@ -1,7 +0,0 @@
-helm upgrade --install --cleanup-on-fail \
-  --namespace homer-system --create-namespace \
-  --set fullnameOverride=homer \
-  homer .
-
-helm uninstall homer --namespace homer-system
-kubectl delete namespace homer-system
